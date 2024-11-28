3dd2496353e595c2ee9dd0d40b7659d3fa905478
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2024-11-28 18:52:26 +0200

    change also

--- index.js
+++ index.js
@@ -3,41 +3,51 @@ import mongoose from 'mongoose';
 import router from './Route/index.js';
 import errorHandler from './Middleware/errorhandler.js';
 import dotenv from 'dotenv';
-import documentation from "./Doc/swagger.json" assert { type:"json"};
-import swaggerUi from "swagger-ui-express"
+import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 dotenv.config();
-import cors from "cors"
 
-const corsOptions ={
-    allowedHeaders: ["Authorization", "Content-Type" ],
-    methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
-    origin:"*",
-}
-//time seconds
+// Dynamic JSON Import
+const loadSwaggerDoc = async () => {
+  const documentation = await import('./Doc/swagger.json', { assert: { type: 'json' } });
+  return documentation.default;
+};
+
+loadSwaggerDoc()
+  .then((documentation) => {
+    app.use('/api_docs', swaggerUi.serve, swaggerUi.setup(documentation));
+  })
+  .catch((err) => {
+    console.error('Error loading Swagger documentation:', err);
+  });
+
+
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // 30 seconds timeout
-    socketTimeoutMS: 45000, // 45 seconds timeout
-    maxPoolSize: 10, // Maintain up to 10 socket connections
-  };
+  serverSelectionTimeoutMS: 30000, 
+  socketTimeoutMS: 45000, 
+  maxPoolSize: 10,
+};
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api_docs",swaggerUi.serve, swaggerUi.setup(documentation))
-app.use('/CaseChain',router);
-mongoose.connect(`${process.env.db}`,options)
-.then(()=>
-{
-    console.log('connected to db');
-})
-.catch(err=>
-    {
-        console.log(err);
-    }
-)
-app.listen(process.env.PORT,()=>
-{
-    console.log(`server is running on port ${process.env.PORT}`);
-})
-app.use(errorHandler)
+
+// Load Swagger Documentation dynamically
+loadSwaggerDoc().then((documentation) => {
+  app.use("/api_docs", swaggerUi.serve, swaggerUi.setup(documentation));
+}).catch(err => {
+  console.error('Error loading Swagger documentation:', err);
+});
+
+app.use('/CaseChain', router);
+
+// MongoDB Connection
+mongoose.connect(process.env.db, options)
+  .then(() => console.log('connected to db'))
+  .catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
+
+app.use(errorHandler);
