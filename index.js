d8ecb501a5a2b56d6ae234e801f60c0a9e653feb
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2024-11-28 09:28:06 +0200

    some changes

--- index.js
+++ index.js
@@ -2,43 +2,40 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './route/index.js';
 import errorHandler from './Middleware/errorHandler.js';
-import documentation from './Doc/swagger.json' assert { type: "json" };
-import swaggerUi from 'swagger-ui-express'
+import swaggerUi from 'swagger-ui-express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
-import cors from "cors"
 
+// Dynamic import for JSON
+const loadSwaggerDoc = async () => {
+  const documentation = await import('./Doc/swagger.json', {
+    assert: { type: 'json' },
+  }).then((mod) => mod.default);
+  return documentation;
+};
 
-const corsOptions ={
-    allowedHeaders: ["Authorization", "Content-Type" ],
-    methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
-    origin:"*",
-}
-//time seconds
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // 30 seconds timeout
-    socketTimeoutMS: 45000, // 45 seconds timeout
-    maxPoolSize: 10, // Maintain up to 10 socket connections
-  };
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use("/api_docs",swaggerUi.serve,swaggerUi.setup(documentation))
-app.use('/CaseChain',router);
-mongoose.connect(`${process.env.db}`,options)
-.then(()=>
-{
-    console.log('connected to db');
-})
-.catch(err=>
-    {
-        console.log(err);
-    }
-)
-app.listen(process.env.PORT,()=>
-{
-    console.log(`server is running on port ${process.env.PORT}`);
-})
-app.use(errorHandler)
+
+loadSwaggerDoc().then((documentation) => {
+  app.use('/api_docs', swaggerUi.serve, swaggerUi.setup(documentation));
+});
+
+app.use('/CaseChain', router);
+
+mongoose.connect(process.env.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('connected to db');
+}).catch((err) => {
+  console.log(err);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+app.use(errorHandler);
